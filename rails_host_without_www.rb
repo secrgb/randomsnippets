8004e1396104eccbdeaab9036f9aaf3cf2018d12
secrgb <jaan@janesmae.com>
2009-12-07 09:43:36 -0700

    adding a comment

--- rails_host_without_www.rb
+++ rails_host_without_www.rb
@@ -2,7 +2,7 @@
 
 def host_without_www(host)
   parts = host.split('.')
-  if parts[0] == 'www' and parts.count <= 3
+  if parts[0] == 'www' and parts.count <= 3 # in case there is a domain like www.tld then the www won't be erased
     host = parts[1..parts.count-1].join('.')
   end
   return host
